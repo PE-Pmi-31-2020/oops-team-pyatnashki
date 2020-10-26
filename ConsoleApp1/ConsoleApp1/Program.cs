7687591685374a2b3c5a93c269a58fdb85ed64c2
maxSlkk <max.slushch@gmail.com>
2020-10-26 15:03:38 +0200

    Added script for creating database.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace ConsoleApp1
@@ -7,12 +8,62 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string connectionString = @"Data Source=.\MSSQLSERVER02;Initial Catalog=FiveOurs;User Id = sa;Password = Aa1234567890";
+            string connectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=FiveOurs;Integrated Security=True";
+
+            CreateDatabase();
 
             ReadData(connectionString);
             
 
+
             Console.Read();
+        }
+
+
+        static void CreateDatabase()
+        {
+            SqlConnection con = new SqlConnection
+                (@"Data Source=localhost\SQLEXPRESS;Initial Catalog=master;Integrated Security=True");
+            string createDatabaseQuery =
+                            "IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'FiveOurs')" +
+                            "BEGIN\n" +
+                            "CREATE DATABASE FiveOurs\n" +
+                            "END\n";
+            SqlCommand cmd = new SqlCommand(createDatabaseQuery, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Creating database script executed successfully.");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error Generated. Details: " + e.ToString());
+            }
+
+            string createTableQuery =
+                            "USE FiveOurs\n" +
+                            "IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='Results' and xtype='U')\n" +
+                            "BEGIN\n" +
+                            "CREATE TABLE Results(\n" +
+                            "res_id INT PRIMARY KEY IDENTITY(1, 1),\n" +
+                            "player_name VARCHAR(50) NOT NULL,\n" +
+                            "game_time INT NOT NULL,\n" +
+                            "number_of_moves INT NOT NULL\n" +
+                            ")\n" +
+                            "END\n";
+            cmd = new SqlCommand(createTableQuery, con);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Creating table script executed successfully.");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error Generated. Details: " + e.ToString());
+            }
+
+            con.Close();
         }
 
         static void ReadData(string connectionString)
@@ -42,5 +93,8 @@ namespace ConsoleApp1
                 reader.Close();
             }
         }
+
+
+
     }
 }
