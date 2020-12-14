adb8a84d5750d5cf069c46ead2d48762ccf9eae5
Andriy Oleksyuk <andre200129@gmail.com>
2020-12-15 00:26:54 +0200

    Added unit-tests

--- Project/UnitTests/UnitTest1.cs
+++ Project/UnitTests/UnitTest1.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using FiveOursDAL.Configurations;
 using FiveOursDAL.Entities;
 using FiveOursDAL.Repository;
+using FiveOursInterface;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using NUnit.Framework;
@@ -31,14 +32,73 @@ namespace NUnitTestProject
             _entitiesRepository = new Repository<IEntity>(_mockDbContext.Object);
         }
 
+        [Test]
+        public void GetAll_Always_MustCallDbSetGetAllMethod()
+        {
+            var mockSet = new Mock<DbSet<IEntity>>();
+            var mockContext = new Mock<ProjectDbContext>();
+            mockContext.Setup(m => m.Set<IEntity>()).Returns(mockSet.Object);
+            var repo = new Repository<IEntity>(mockContext.Object);
+
+            repo.GetAll();
+
+            mockSet.Verify(m => m.AsQueryable(), Times.Once());
+        }
 
         [Test]
-        public void Test1()
+        public void GetAll_HavingListOfEntities_ReturnsListOfEntities()
         {
             var expectedEntities = new List<IEntity>().AsQueryable();
             var entities = _entitiesRepository.GetAll();
 
             Assert.AreEqual(entities, expectedEntities);
+        }
+
+        [Test]
+        public void AddUser_Always_MustCallDbSetAddMethod()
+        {
+            var mockSet = new Mock<DbSet<IEntity>>();
+            var mockContext = new Mock<ProjectDbContext>();
+            mockContext.Setup(m => m.Set<IEntity>()).Returns(mockSet.Object);
+            var repo = new Repository<IEntity>(mockContext.Object);
+
+            repo.Add(new Result());
+
+            mockSet.Verify(m => m.Add(It.IsAny<IEntity>()), Times.Once());
+        }
+
+        [Test]
+        public void LogFilePath_Always_MustBeLogTxt()
+        {
+            var logger = new FileLogger();
+            string expectedPath = "log.txt";
+
+            string path = logger.LogFilePath;
+
+            Assert.AreEqual(path, expectedPath);
+
+        }
+
+        [Test]
+        public void Result_Always_MustBeInitializedWithCertainData()
+        {
+            int expectedId = 1;
+            string expectedName = "first";
+            int expectedNumber = 5;
+            int expectedTime = 100;
+
+            var result = new Result()
+            {
+                ResultId = 1,
+                Name = "first",
+                NumberOfMoves = 5,
+                Time = 100
+            };
+
+            Assert.AreEqual(result.ResultId, expectedId);
+            Assert.AreEqual(result.Name, expectedName);
+            Assert.AreEqual(result.NumberOfMoves, expectedNumber);
+            Assert.AreEqual(result.Time, expectedTime);
         }
     }
 }
